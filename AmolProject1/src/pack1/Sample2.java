7f0c1eceb702642ec5d0ae57971d3444dfa17791
Chandra-Laptop <Chandra-Laptop@DESKTOP-K5247UH>
2018-07-11 17:57:20 +0530

    added msgs

--- AmolProject1/src/pack1/Sample2.java
+++ AmolProject1/src/pack1/Sample2.java
@@ -1,7 +1,13 @@
 package pack1;
 
 public class Sample2 {
-  
-  System.out.println("hello world sample2");
+	
+public static void main(String ar[])
 
+{
+	System.out.println("hello sample2");
+	System.out.println("nkvjsnk");
+System.out.println("jdskjcnj");
+
+}
 }
